a081b70d68b5634c1fc1fef5a4d3fdefa12f85d6
Joe Reiff <jcreiff@gmail.com>
2015-06-10 13:59:51 -0400

    edit fixtures to pass controller tests

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,10 +1,10 @@
 class Teacher < ActiveRecord::Base
 
   has_secure_password
-  has_many :students, dependent: :restrict_with_error
+  has_many :students
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
-  validates :password, presence: true
+  validates :password_digest, presence: true
 
 end
