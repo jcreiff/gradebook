452782c282766c6d52e11031fdd0da00bf1eb50c
Joe Reiff <jcreiff@gmail.com>
2015-06-10 14:06:29 -0400

    test email uniqueness validation

--- test/models/teacher_test.rb
+++ test/models/teacher_test.rb
@@ -1,7 +1,16 @@
 require 'test_helper'
 
 class TeacherTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
+  setup do
+    @one = teachers(:one)
+    @two = teachers(:two)
+  end
+
+  test "email must be unique" do
+    t_one = Teacher.new(name: @one.name, email: Faker::Internet.email, password_digest: @one.password_digest)
+    t_two = Teacher.new(name: @two.name, email: @two.email, password_digest: @two.password_digest)
+
+    assert t_one.save
+    refute t_two.save
   end
 end
