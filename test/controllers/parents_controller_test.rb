c4d245da60e31339dbb4fd3e69c62b6d76dad1e0
Joe Reiff <jcreiff@gmail.com>
2015-06-11 08:09:10 -0400

    comment out tests for functions that were removed from parent/student authorization

--- test/controllers/parents_controller_test.rb
+++ test/controllers/parents_controller_test.rb
@@ -6,41 +6,41 @@ class ParentsControllerTest < ActionController::TestCase
     session[:user_id] = @parent.id
     session[:parent_id] = @parent.id
   end
+  #
+  # test "should get index" do
+  #   get :index
+  #   assert_response :success
+  #   assert_not_nil assigns(:parents)
+  # end
+  # #
+  # test "should get new" do
+  #   get :new
+  #   assert_response :success
+  # end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:parents)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should create parent" do
-    assert_difference('Parent.count') do
-      post :create, parent: { email: Faker::Internet.email, name: @parent.name, password: "password", student_id: @parent.student_id }
-    end
-
-    assert_redirected_to parent_path(assigns(:parent))
-  end
+  # test "should create parent" do
+  #   assert_difference('Parent.count') do
+  #     post :create, parent: { email: Faker::Internet.email, name: @parent.name, password: "password", student_id: @parent.student_id }
+  #   end
+  #
+  #   assert_redirected_to parent_path(assigns(:parent))
+  # end
 
   test "should get edit" do
     get :edit, id: @parent
     assert_response :success
   end
 
-  test "should update parent" do
-    patch :update, id: @parent, parent: { email: @parent.email, name: @parent.name, password: "password", student_id: @parent.student_id }
-    assert_redirected_to parent_path(assigns(:parent))
-  end
-
-  test "should destroy parent" do
-    assert_difference('Parent.count', -1) do
-      delete :destroy, id: @parent
-    end
-
-    assert_redirected_to parents_path
-  end
+  # test "should update parent" do
+  #   patch :update, id: @parent, parent: { email: @parent.email, name: @parent.name, password: "password", student_id: @parent.student_id }
+  #   assert_redirected_to parent_path(assigns(:parent))
+  # end
+  #
+  # test "should destroy parent" do
+  #   assert_difference('Parent.count', -1) do
+  #     delete :destroy, id: @parent
+  #   end
+  #
+  #   assert_redirected_to parents_path
+  # end
 end
