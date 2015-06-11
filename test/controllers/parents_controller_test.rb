fcc167205cd03eba048eddb7c142dc041fc8ef6e
Joe Reiff <jcreiff@gmail.com>
2015-06-11 09:45:11 -0400

    edit controller test setup for teacher access

--- test/controllers/parents_controller_test.rb
+++ test/controllers/parents_controller_test.rb
@@ -3,44 +3,44 @@ require 'test_helper'
 class ParentsControllerTest < ActionController::TestCase
   setup do
     @parent = parents(:one)
-    session[:user_id] = @parent.id
-    session[:parent_id] = @parent.id
+    session[:user_id] = teachers(:one).id
+    session[:teacher_id] = teachers(:one).id
+  end
+
+  test "should get index" do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:parents)
   end
   #
-  # test "should get index" do
-  #   get :index
-  #   assert_response :success
-  #   assert_not_nil assigns(:parents)
-  # end
-  # #
-  # test "should get new" do
-  #   get :new
-  #   assert_response :success
-  # end
+  test "should get new" do
+    get :new
+    assert_response :success
+  end
 
-  # test "should create parent" do
-  #   assert_difference('Parent.count') do
-  #     post :create, parent: { email: Faker::Internet.email, name: @parent.name, password: "password", student_id: @parent.student_id }
-  #   end
-  #
-  #   assert_redirected_to parent_path(assigns(:parent))
-  # end
+  test "should create parent" do
+    assert_difference('Parent.count') do
+      post :create, parent: { email: Faker::Internet.email, name: @parent.name, password: "password", student_id: @parent.student_id }
+    end
+
+    assert_redirected_to parent_path(assigns(:parent))
+  end
 
   test "should get edit" do
     get :edit, id: @parent
     assert_response :success
   end
 
-  # test "should update parent" do
-  #   patch :update, id: @parent, parent: { email: @parent.email, name: @parent.name, password: "password", student_id: @parent.student_id }
-  #   assert_redirected_to parent_path(assigns(:parent))
-  # end
-  #
-  # test "should destroy parent" do
-  #   assert_difference('Parent.count', -1) do
-  #     delete :destroy, id: @parent
-  #   end
-  #
-  #   assert_redirected_to parents_path
-  # end
+  test "should update parent" do
+    patch :update, id: @parent, parent: { email: @parent.email, name: @parent.name, password: "password", student_id: @parent.student_id }
+    assert_redirected_to parent_path(assigns(:parent))
+  end
+
+  test "should destroy parent" do
+    assert_difference('Parent.count', -1) do
+      delete :destroy, id: @parent
+    end
+
+    assert_redirected_to parents_path
+  end
 end
