f74ebb952acb67c915ef5ca02638cc2bc9a665c0
Joe Reiff <jcreiff@gmail.com>
2015-06-11 03:41:06 -0400

    create seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+teacher = Teacher.create!(name: "Test Teacher", email: "test@teacher", password: "test")
+student = Student.create!(teacher_id: teacher.id, name: "Test Student", email: "test@student", password: "test")
+parent = Parent.create!(student_id: student.id, name: "Test Parent", email: "test@parent", password: "test")
+grade = Grade.create!(student_id: student.id, assignment_name: "Test", score: 100, date: Date.today)
+
+5.times do
+  Teacher.create!(name: Faker::Name.name, email: Faker::Internet.email, password: "test")
+end
+
+t_ids = Teacher.all.map {|t| t.id}
+
+50.times do
+  Student.create!(teacher_id: t_ids.sample, name: Faker::Name.name, email: Faker::Internet.email, password: "test")
+end
+
+s_ids = Student.all.map {|s| s.id}
+
+100.times do
+  Parent.create!(student_id: s_ids.sample, name: Faker::Name.name, email: Faker::Internet.email, password: "test")
+end
+
+assignments = ["Test", "Essay", "Quiz", "Presentation", "Homework"]
+
+500.times do
+  Grade.create!(student_id: s_ids.sample, assignment_name: assignments.sample,
+      score: Faker::Number.number(2), date: Faker::Date.between(180.days.ago, Date.today))
+end
